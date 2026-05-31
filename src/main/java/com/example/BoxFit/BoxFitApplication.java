829fb15186fc624eefb7372adcf03c469d453d65
baonoob1011 <trandinhbao222@gmail.com>
2026-05-31 12:03:46 +0700

    initial project

---
+++ src/main/java/com/example/BoxFit/BoxFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.BoxFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoxFitApplication.class, args);
+	}
+
+}
